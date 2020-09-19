1d35151486f118a9bf92267d78ce55a91591db93
lpizarro <lpizarro2391@github.com>
2020-09-18 23:55:27 -0600

    final

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,7 +1,7 @@
-var mdCalculadora =(function (document,undefined)
+var mdCalculadora =(function (document,undefined){
     var error_op = "La operacion solicitada no existe";
     var newVal = false,
-        maxLen = 8,
+        maxLen = 9,
         nOp1 = 0,
         nOp2 = 0,
         hasPoint = false;
@@ -31,20 +31,6 @@ function initVars(){
     hasPoint = false;
     screenval = "0",
     idBOp = "";
-};
-
-var domBtns;
-
-function initVars(){
-    newVal = false;
-    maxLen = 8;
-    nOp1 = 0;
-    nOp2 = 0;
-    result = 0;
-    hasPoint = false;
-    screenval = "0",
-    idBOp = "";
-
 }
 
 var getBtns = function(){
@@ -65,8 +51,10 @@ var events = {
                 writeDisplay(screenval);
                 break;
                 case btnsId.btn_sign: 
-                initVars();
+                setSign();
                 writeDisplay(screenval);
+                break;
+                case btnsId.btn_raiz:
                 break;
                 case btnsId.btn_punto:
                     addPoint();
@@ -80,28 +68,146 @@ var events = {
                 case btnsId.btn_div: 
                     addBasicOps(this.id);
                     break;
-                    default:
-                        addNumber(this.id);
+                default:
+                    addNumber(this.id);
         }
     }
 }
 
 function writeDisplay(value){
-    if (value.long > maxLength) value = "ERROR";
+    if (value.long > maxLen) value = "ERROR";
     document.getElementById("display").innerHTML = value;
 }
 
-
-
-
-
-
-
+function solveOp(){
+    if (screnVal.endsWith(".")) screenval.substring(0,screnVal.length-1);
+    if (screenVal.length > maxLen) return;
+    if (idBOp ==""){
+        return;
+    } else {
+        nOp2 = Number(screnVal);
+        switch (idBOp) {
+            case btnsId.btn_mas:
+                result = add(nOp1, nOp2);
+                break;
+            case btnsId.btn_menos:
+                result = substract(nOp1, nOp2);
+                break;
+            case btnsId.btn_por:
+                result = multiply(nOp1, nOp2);
+                break;
+            case btnsId.btn_div:
+                result = divide(nOp1, nOp2);
+                break;
+            default:
+                alert(error_op);
+                return;
+        }
+        nOp2 = 0;
+        screnVal = String(result);
+        nOp1 = result;
+    }
+    if(screenVal.search(".") == -1) maxLen = 9;
+    else maxLen = 10;
+    newVal = true;
+    writeDisplay(screnVal);
+    hasPoint = false;
+    idBOp = "";
+    maxLen = 9;
 }
 
+function setSign(){
+    result = Number(screnVal);
+    result = -1 * result;
+    screnVal = String(result);
+}
 
+function addPoint(){
+    if (hasPoint == true) return;
+    if (screnVal.length > maxLen) return;
+    if (newVal == false && screnVal == "0") screnVal = "0";
+    if (newVal == true || screnVal =="0") screnVal = "0";
+    screnVal = screnVal + ".";
+    writeDisplay(screnVal);
+    maxLen = 10;
+    hasPoint = true;
+    newVal = false;
+}
 
+function addNumber(id){
+    if (screenval.length> maxLen) return;
+    if( newVal == false && screnVal.length ==maxLen) return;
+    if (newVal == true || screnVal == "0") screnVal = "";
+    screnVal = screnVal + id;
+    writeDisplay(screnVal);
+    newVal = false;
+}
+function addBasicOps(id){
+    if (screenval.endsWith(".")) screenval = screenVal.substr(0,screenVal.length-1);
+    if(screenVal.length > maxLen)return;
+    if (idBOp == "") {
+        nOp1 = Number(screenVal);
+    } else{
+        nOp2 = Number(screenVal);
+        switch(idBOp){
+            case btnsId.btn_mas:
+                result = add(nOp1, nOp2);
+                break;
+            case btnsId.btn_menos:
+                result = substract(nOp1, nOp2);
+                break;
+            case btnsId.btn_por:
+                result = multiply(nOp1, nOp2);
+                break
+            case btnsId.btn_div:
+                result = divide(nOp1, nOp2);
+                break;
+            default:
+                alert(error_op);
+                return;
+        }
+        if (id != idBOp) {
+            idBOp = id;
+        }
+        nOp2 = 0;
+        screenVal = String(result);
+        nOp1 = result;
+    }
+    if (screenval.search(".") == -1)maxLen = 9;
+    else maxLen = 10;
+    newVal = true;
+    writeDisplay(screenVal);
+    hasPoint = false;
+    idBOp = id;
+}
+// INI operaciones suma, resta, multiplicacion, division y raiz cuadrada 
+function add (v1, v2){
+    return v1 + v2;
+}
 
+function substract(v1, v2){
+    return v1 - v2;
+}
 
+function multiply (v1, v2){
+    return v1 * v2;
+}
+function divide (v1,v2){
+    return v1 / v2;
+}
 
-)
+// fin operaciones suma, resta , multiplicacion, division y raiz cuadrada
+
+var initialize = function(){
+    initVars()
+    getBtns();
+    subscribeEvents();
+}
+
+return{
+    init: initialize
+}
+
+})(document);
+
+mdCalculadora.init();
